f53855221bb42dfcf551e4e947018650f2638a09
wangqing <isswangqing@163.com>
2015-01-16 23:10:28 +0800

    title的布局

--- Android/src/net/ui/UserListActivity.java
+++ Android/src/net/ui/UserListActivity.java
@@ -1,5 +1,28 @@
 package net.ui;
 
-public class UserListActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
+
+import com.example.netdatatransfer.R;
+
+public class UserListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//隐藏标题栏  
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);  
+		setContentView(R.layout.user_list);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
 
 }
