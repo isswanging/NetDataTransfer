a0b7ec6fecf66d0b81ee0bc97dea3915ea9d6b42
wangqing <isswangqing@163.com>
2015-05-01 19:41:14 +0800

    修正二维码取景框位置偏下

--- Android/src/com/Zxing/Demo/camera/CameraManager.java
+++ Android/src/com/Zxing/Demo/camera/CameraManager.java
@@ -237,7 +237,7 @@ public final class CameraManager {
         height = MAX_FRAME_HEIGHT;
       }
       int leftOffset = (screenResolution.x - width) / 2;
-      int topOffset = (screenResolution.y - height) / 2;
+      int topOffset = (screenResolution.y - height) / 2 - 100;
       framingRect = new Rect(leftOffset, topOffset, leftOffset + width, topOffset + height);
       Log.d(TAG, "Calculated framing rect: " + framingRect);
     }
