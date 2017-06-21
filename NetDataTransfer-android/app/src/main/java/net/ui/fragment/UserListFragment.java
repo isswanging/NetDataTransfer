20c140f6fad9d2af526f283f04c80ecc13b7dde9
wangqing <isswangqing@163.com>
2017-06-21 23:36:56 +0800

    优化界面

--- NetDataTransfer-android/app/src/main/java/net/ui/fragment/UserListFragment.java
+++ NetDataTransfer-android/app/src/main/java/net/ui/fragment/UserListFragment.java
@@ -298,7 +298,7 @@ public class UserListFragment extends BaseFragment {
                     default:
                         break;
                 }
-                hideMenu();
+                mPopupWindow.dismiss();
             }
         };
         moreMenu.setOnClickListener(new View.OnClickListener() {
